f888b2decf33bcb67a6ca81679083af14e09d7ba
Mikeland <mikedoslands@gmail.com>
2025-12-16 22:35:16 +0000

    Fixed language selector clickable are alignment

--- common.js
+++ common.js
@@ -6,7 +6,7 @@ function switchTheme() {
 const savedTheme = localStorage.getItem("theme") || "light";
 if (savedTheme === "dark") {
   document.body.classList.add("light-mode");
-  document.querySelector("#theme-switch").checked = true; // sync checkbox
+  document.querySelector("#theme-switch").checked = true;
 } else {
   document.body.classList.remove("light-mode");
   document.querySelector("#theme-switch").checked = false;
